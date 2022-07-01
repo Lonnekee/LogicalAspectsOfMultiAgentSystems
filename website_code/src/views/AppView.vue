30f885ceb878252ae8f634bc53f0352964b59ad6
Lonnekee <lonneke.pulles@gmail.com>
2022-07-01 14:13:52 +0200

    Update build

--- website_code/src/views/AppView.vue
+++ website_code/src/views/AppView.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="application">
-    <h1>This is the page that will host the application.</h1>
+    <p>
+      The application has been written in Python and can be downloaded
+      <a href="https://github.com/Lonnekee/LogicalAspectsOfMultiAgentSystems/tree/main/py_code">here</a>.
+    </p>
   </div>
 </template>
-
-<script setup lang="ts">
-import {PythonShell} from 'python-shell';
-
-PythonShell.runString(
-    'from tkinter import *; Tk().mainloop()',
-    undefined,
-    function(err: any) {
-      if (err) throw err;
-      console.log('finished');
-    }
-)
-</script>
 
 <style>
 @media (min-width: 1024px) {
--- website_code/src/views/ReportView.vue
+++ website_code/src/views/ReportView.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="report">
-    <iframe frameborder=”0″
-            width=”100%″ height=”100%″
-            src=”https://lonnekee.github.io/LogicalAspectsOfMultiAgentSystems/report.pdf”>
+    <iframe class="pdf" width="100%" height="100%"
+            src="https://lonnekee.github.io/LogicalAspectsOfMultiAgentSystems/report.pdf">
     </iframe>
   </div>
 </template>
+
+<script>
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
@@ -13,6 +16,16 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+  }
+
+  .pdf {
+    height: 900px;
+  }
+}
+
+@media (max-width: 1024px) {
+  .pdf {
+    height: 100%;
   }
 }
 </style>
