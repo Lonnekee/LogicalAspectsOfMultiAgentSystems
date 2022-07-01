d2d8b9b22fc8b921d819689c3924143e632fd509
Lonnekee <lonneke.pulles@gmail.com>
2022-07-01 13:15:37 +0200

    Update build

--- website_code/src/views/AppView.vue
+++ website_code/src/views/AppView.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="application">
     <h1>This is the page that will host the application.</h1>
+
+    <script setup lang="ts">
+      import {PythonShell} from 'python-shell';
+
+      PythonShell.runString(
+          'from tkinter import *; Tk().mainloop()',
+          undefined,
+          function(err: any) {
+            if (err) throw err;
+            console.log('finished');
+          }
+      )
+    </script>
   </div>
 </template>
 
